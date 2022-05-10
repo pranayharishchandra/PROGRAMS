2ff966fa3ce09515cb619ddf84f2a0126f12ab5f
PRANAY HARISHCHANDRA <44168141+pranayharishchandra@users.noreply.github.com>
2022-05-10 23:30:37 +0530

    Update 3 - prime number - 9 may.cpp

--- 3 - prime number - 9 may.cpp
+++ 3 - prime number - 9 may.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin >> n;
     bool flag = true;
-    for (int i = 2; i < sqrt(n) ; i++){
+    for (int i = 2; i <= sqrt(n) ; i++){  //<-- this is correct        //for (int i = 2; i < sqrt(n) ; i++){ <-- this is wrong
         if (n%i == 0){
             flag = false;
             cout << "not-prime" << endl;
